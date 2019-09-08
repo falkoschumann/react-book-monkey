5e7cb9ecdaebf928318d3c09423e01b2054bc7e9
Falko Schumann <falko.schumann@muspellheim.de>
2019-09-08 12:34:54 +0200

    Import korrigiert

--- src/components/BookDetails.tsx
+++ src/components/BookDetails.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RouteComponentProps } from 'react-router';
+import { RouteComponentProps } from 'react-router-dom';
 
 import { Book } from '../api/Book';
 import BookStore from '../flux/BookStore';
